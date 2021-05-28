7a23c8f2bb91763a874dea07f8a369e417edf933
Beast51 <7007500@gmail.com>
2021-05-28 17:34:41 +0300

    add react-jss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  app: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingTop: "16px",
+  },
+  card: {
+    width: "288px",
+    height: "288px",
+    backgroundColor: "lightcoral",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  word: {
+    fontFamily: "Roboto, sans-serif",
+    fontSize: "38px",
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      <div className={classes.card}>
+        <p className={classes.word}>Apple</p>
+      </div>
     </div>
   );
 }
