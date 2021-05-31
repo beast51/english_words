7bf14596d2f0b9e551b930242e22bafd55f70099
Beast51 <7007500@gmail.com>
2021-05-31 17:50:09 +0300

    start making slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
 import { createUseStyles } from "react-jss";
+import Slider from "./Components/Slider/Slider";
 
 const useStyles = createUseStyles({
+  "@global": {
+    body: {
+      margin: 0,
+    },
+  },
+  slider: {
+    overflow: "hidden",
+  },
+  cardLine: {
+    display: "flex",
+  },
   app: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    paddingTop: "16px",
-  },
-  card: {
-    width: "288px",
-    height: "288px",
-    backgroundColor: "lightcoral",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  word: {
-    fontFamily: "Roboto, sans-serif",
-    fontSize: "38px",
+    padding: "16px",
   },
 });
+
+const data: Array<{ [key: string]: string }> = [
+  { Apple: "Яблоко" },
+  { Grape: "Виноград" },
+];
 
 function App() {
   const classes = useStyles();
   return (
     <div className={classes.app}>
-      <div className={classes.card}>
-        <p className={classes.word}>Apple</p>
+      <div className={classes.slider}>
+        <div className={classes.cardLine}>
+          <Slider data={data} />
+        </div>
       </div>
     </div>
   );
